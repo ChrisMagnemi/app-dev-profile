430c1ccbeffd4b9bda544d56188151225acfa9e4
chrismag412 <magnemi@bc.edu>
2018-06-12 22:33:09 -0400

    initial xcode project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Chris Magnemi on 6/12/18.
+//  Copyright © 2018 Chris Magnemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
